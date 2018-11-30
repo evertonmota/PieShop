798aaea16071d1ea82fdd1d312a11217d18a4d7d
Everton Mota <mcostagt@gmail.com>
2018-11-30 18:23:42 -0200

    add Bootstrap

--- PieShopping/PieShopping/Models/MockPieRepository.cs
+++ PieShopping/PieShopping/Models/MockPieRepository.cs
@@ -14,11 +14,11 @@ namespace PieShopping.Models
             {
                 return new List<Pie>
                 {
-                    new Pie {PieId = 1, Name="Strawberry Pie", Price=15.95M, ShortDescription=""},
-                    new Pie {PieId = 2, Name="Cheese cake", Price=18.95M, ShortDescription=""},
-                    new Pie {PieId = 3, Name="Rhuarb Pie", Price=25.95M, ShortDescription=""},
-                    new Pie {PieId = 4, Name="Pumpkin Pie", Price=19.95M, ShortDescription=""},
-                    
+                    new Pie {PieId = 1, Name="Strawberry Pie", Price=15.95M, ShortDescription="", ImageThumbnaiUrl="https://gillcleerenpluralsight.blob.core.windows.net/files/strawberrypiesmall.jpg"},
+                    new Pie {PieId = 2, Name="Cheese cake", Price=18.95M, ShortDescription="", ImageThumbnaiUrl="https://gillcleerenpluralsight.blob.core.windows.net/files/cheesecakesmall.jpg"},
+                    new Pie {PieId = 3, Name="Rhuarb Pie", Price=25.95M, ShortDescription="",ImageThumbnaiUrl="https://gillcleerenpluralsight.blob.core.windows.net/files/rhubarbpiesmall.jpg"},
+                    new Pie {PieId = 4, Name="Pumpkin Pie", Price=19.95M, ShortDescription="", ImageThumbnaiUrl="https://gillcleerenpluralsight.blob.core.windows.net/files/pumpkinpiesmall.jpg"},
+
                 };
             }
         }
